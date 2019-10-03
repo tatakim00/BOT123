ab9bb268f7e9a3b6d30f4de01d7fe4396fc8a415
Pinco <puffored@gmail.com>
2019-10-03 19:11:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 const PREFIX ='!';
 
 function emoji (id){
     return clientInformation.emojis.get(id).toString ();
 
 }
-client.on('ready',() => {
+bot.on('ready',() => {
     console.log("I'm ready to send nudes!");
 })
 
-client.on('guildMemberAdd', message => {
+bot.on('guildMemberAdd', member => {
     const channel = member.guild.channels.find(channel => channel.name === "♡┆benvenuti")
     if(!channel)return
     channel.send(`Benvenuto ${member}, siamo felici di accoglierti in ,:cherry_blossom:𝕃𝕠𝕝𝕚𝕤𝕎𝕠𝕣𝕝𝕕:cherry_blossom. Non abusare delle bambine e buona permanenza! <:zhoulamerda:608396991436685344>`);
 })
-client.login(process.env.BOT_TOKEN);
+
+bot.on('message', message=>{
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'play':
+        break ;
+    }
+})
+bot.login(process.env.BOT_TOKEN);
