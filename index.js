22fd003539de22af051e6626b7aa10b1cab1d139
Pinco <puffored@gmail.com>
2019-10-03 18:20:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = process.env.token;
+const token = 'NjI4MjI1MzE5Mjc4NDc3MzEy.XZYOeg.mlpAbBANKhJCgwKGuzO6PyNn-qo';
 
 bot.on('ready',() => {
     console.log("I'm ready to ban!");
