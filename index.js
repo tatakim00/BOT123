c80d8da5411575e3371b50c534f0c8e911be8c86
Pinco <puffored@gmail.com>
2019-10-03 23:28:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ https://i.kym-cdn.com/photos/images/original/001/241/247/c04.gif`);
 bot.on('message', message => {
     if (message.content.startsWith(PREFIX + "aot meme")) {
       const embed = new Discord.RichEmbed()
-      .setImage("https://i.imgur.com/H6kfgP7.mp4")
+      .setImage("https://i.imgur.com/H6kfgP7.gif")
       .setFooter("Powered by pinco.il.mago")
       .setTimestamp()
       message.channel.send({embed})
