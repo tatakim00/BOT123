4e13c629a39083de1d9503b12d83baef7c9742ef
Pinco <puffored@gmail.com>
2019-10-04 00:24:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on('message', message =>{
                     break;
                 };
                 case 3: {
-                    const embed = new DIscord.RichEmbed()
+                    const embed = new Discord.RichEmbed()
                     .setImage("https://cdn.myanimelist.net/s/common/uploaded_files/1452670440-e83dfccf7c336129d27c54ccd4d83242.gif")
                     .setFooter("Powered by pinco.il.mago")
                     .setTimestamp()
