fa483f4c9a7788134aae408b761ff17a9e2ff6ee
Pinco <puffored@gmail.com>
2019-10-03 18:18:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = process.env.token;
 
-client.on('ready',() => {
+bot.on('ready',() => {
     console.log("I'm ready to ban!");
 })
 
-client.on('message', message => {
+bot.on('message', message => {
     if(message.content === 'hello') message.reply('Hello Tatanudes');
 })
-client.login(token).catch(err => console.log(err));
+bot.login(token).catch(err => console.log(err));
