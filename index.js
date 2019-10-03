561d4441dee16a7333502bcc81c503195c94cff4
Pinco <puffored@gmail.com>
2019-10-03 19:05:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const PREFIX ='!';
 
 function emoji (id){
     return clientInformation.emojis.get(id).toString ();
 
 }
 client.on('ready',() => {
-    console.log("I'm ready to ban!");
+    console.log("I'm ready to send nudes!");
 })
 
-client.on('message', message => {
-    if(message.content === 'hello') message.reply('Hello Tatanudes <:zhoulamerda:608396991436685344>');
+client.on('guildMemberAdd', message => {
+    const channel = member.guild.channels.find(channel => channel.name === "♡┆benvenuti")
+    if(!channel)return
+    channel.send(`Benvenuto ${member}, siamo felici di accoglierti in ,:cherry_blossom:𝕃𝕠𝕝𝕚𝕤𝕎𝕠𝕣𝕝𝕕:cherry_blossom. Non abusare delle bambine e buona permanenza! <:zhoulamerda:608396991436685344>`);
 })
 client.login(process.env.BOT_TOKEN);
