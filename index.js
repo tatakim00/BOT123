24252399e3ef16d3b3d8f8b34722004cfd9fcef5
Pinco <puffored@gmail.com>
2019-10-04 15:44:09 +0200

    sa

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const PREFIX ='kk!';
 
 var fs = require('fs');
 var commandlist = fs.readFileSync('Kanna Kamui Help.txt', 'utf8');
+var meme = fs.readFileSync('meme.txt', 'utf8');
+function emoji (id) { return clientInformation.emojis.get(id).toString (); }
 
-function emoji (id){
-    return clientInformation.emojis.get(id).toString ();
+bot.on('ready',() => { console.log("I'm ready to send nudes!"); } )
 
-}
-bot.on('ready',() => {
-    console.log("I'm ready to send nudes!");
-})
 bot.on('guildMemberAdd', member => {
     const channel = member.guild.channels.find(channel => channel.name === "♡┆benvenuti")
     if(!channel)return
@@ -38,177 +35,22 @@ bot.on('message', message =>{
 bot.on('message', message =>{
     if(message.content.startsWith(PREFIX + "aot meme")){
         number = 20;
-        var random = Math.floor (Math.random() * (number - 1 + 1)) + 1;
-            switch(random){
-                case 1: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://i.imgur.com/H6kfgP7.gif")
-                    .setFooter("Powered by pinco.il.mago")
+        var random = Math.floor (Math.random() * (number));
+        
+        /*= new String (random);*/
+        
+        const embed = new Discord.RichEmbed()
+                    .setImage(meme[random])
+                    .setFooter('Powered by pinco.il.mago')
                     .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                };
-                case 2: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://media3.giphy.com/media/GyChnsTnX8bDi/giphy.gif")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                };
-                case 3: {const embed = new Discord.RichEmbed()
-                    .setImage("https://cdn.myanimelist.net/s/common/uploaded_files/1452670440-e83dfccf7c336129d27c54ccd4d83242.gif")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                };
-                case 4: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://i.kym-cdn.com/photos/images/original/001/241/247/c04.gif")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                };
-                case 5: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("http://25.media.tumblr.com/7a30e9bf8a4eba2cf1f466de90f82471/tumblr_ms3dt2Hz5A1rzjb4go1_500.gif")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                }
-                case 6: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://media0.giphy.com/media/xNqHay7nIhWoM/giphy.gif")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                }
-                case 7: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("http://25.media.tumblr.com/7a30e9bf8a4eba2cf1f466de90f82471/tumblr_ms3dt2Hz5A1rzjb4go1_500.gif")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                }
-                case 8: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://i.imgflip.com/2rg19t.jpg")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                }
-                case 9: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaEKeP6hnhmI5AJucOjB-cJ_3hIfqHAlOI0x149nvc2kI-7E08")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break; 
-                }
-                case 10: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqaWeI8pJrOFcO-EHygFDmnJkYBKcB0QZi6WgikJp2ziG_7KUu5Q")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                }
-                case 11: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4OLnljxg7uezdx3t1UDSgezvtINwBl5_WtVs0F1q1ioKK_JAQ5A")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                }
-                case 12: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://i.kym-cdn.com/photos/images/newsfeed/000/641/238/b99.jpg")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                }
-                case 13: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://i.kym-cdn.com/photos/images/original/000/586/391/df3.jpg")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                }
-                case 14: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_I5It5BQK4PNfcGqxjVvoLhxWihi1itME-I8nOzrUvEH8LLNC")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                }
-                case 15: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://i.kym-cdn.com/photos/images/newsfeed/001/131/532/71d.png")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;  
-                }
-                case 16: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://i.imgur.com/fqXUlkc.gif")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                }
-                case 17: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://data.whicdn.com/images/212845263/original.gif")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break; 
-                }
-                case 18: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAryzNCyNrYv4UFPpcpg_pFQp9HLARnzKZenYI6CZ9IggvN4wu")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break; 
-                }
-                case 19: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("http://37.media.tumblr.com/c48924c62d16c191260166a30ae394e7/tumblr_n48hezByEP1sg146vo1_250.gif")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break;
-                }
-                case 20: {
-                    const embed = new Discord.RichEmbed()
-                    .setImage("https://i.kym-cdn.com/photos/images/newsfeed/000/586/078/d61.gif")
-                    .setFooter("Powered by pinco.il.mago")
-                    .setTimestamp()
-                    message.channel.send({embed})
-                    break; 
-                }
-            }
+                    message.channel.send(embed)
+    }
+    if(message.content.startsWith(PREFIX + "fuoco")){
+        number = 7;
+        var random = Math.floor(Math.random()*(number));
+        const ArrayGif=['https://media1.giphy.com/media/yRnA9pPlLZOY8/source.gif',
+        'https://mondogif.altervista.org/wp-content/uploads/2017/10/%E2%80%9CQuesta-%C3%A8-benzina-io-mi-d%C3%B2-fuoco%E2%80%9D-in-GIF-animata-2.gif']
     }
 });
 
-bot.on('message', message=>{
-    let args = message.content.substring(PREFIX.length).split(" ");
-
-    switch(args[0]){
-        case 'play':
-        break ;
-    }
-})
 bot.login(process.env.BOT_TOKEN);
